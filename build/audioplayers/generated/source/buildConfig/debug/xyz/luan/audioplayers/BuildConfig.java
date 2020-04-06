f85846229bca2a0bdc069b5b39d91b8a0d35c5c0
Sumit Budhiraja <48406445+Sumit-Budhiraja@users.noreply.github.com>
2020-04-06 21:15:07 +0530

    yes

--- build/audioplayers/generated/source/buildConfig/debug/xyz/luan/audioplayers/BuildConfig.java
+++ build/audioplayers/generated/source/buildConfig/debug/xyz/luan/audioplayers/BuildConfig.java
@@ -5,6 +5,11 @@ package xyz.luan.audioplayers;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "xyz.luan.audioplayers";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
   public static final String APPLICATION_ID = "xyz.luan.audioplayers";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
